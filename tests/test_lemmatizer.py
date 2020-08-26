1db7b5a9332bd5443e78fd3458530b35228c96fa
linuxscout <taha_zerrouki@hotmail.com>
2020-08-26 17:22:11 +0100

    Add lemmatization feature

---
+++ tests/test_lemmatizer.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  test_lemmatizer.py
+#  
+#  Copyright 2020 zerrouki <zerrouki@majd4>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import sys
+import pprint
+import pyarabic.araby as araby
+sys.path.append('../')
+import qalsadi.lemmatizer 
+
+def main(args):
+    # #test syn
+    text = u"إلى البيت"
+    text = u"""هل تحتاج إلى ترجمة كي تفهم خطاب الملك؟ اللغة "الكلاسيكية" (الفصحى) موجودة في كل اللغات وكذلك اللغة "الدارجة" .. الفرنسية التي ندرس في المدرسة ليست الفرنسية التي يستخدمها الناس في شوارع باريس .. وملكة بريطانيا لا تخطب بلغة شوارع لندن .. لكل مقام مقال
+    """
+    result = []
+    lemmer = qalsadi.lemmatizer.Lemmatizer()
+    #~ result = lemmer.analyze_text(text)
+    #~ lemmas = lemmer.get_lemmas(result)
+    lemmas = lemmer.lemmatize_text(text, return_pos=True)
+    # the result contains objects
+    pprint.pprint(result)
+    tokens = araby.tokenize(text)
+    pprint.pprint(list(zip(tokens, lemmas)))
+    for tok in tokens:
+        result = lemmer.lemmatize(tok, return_pos=True)
+        #~ lemma, pos = lemmer.lemmatize(tok, return_pos=True)
+        print(tok,result)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
